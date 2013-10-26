e1b1fe8bb4ae30cc102920f45c03da483cf3f575
wangwy1105 <wangwy1105@gmail.com>
2013-10-25 21:55:56 -0400

    seems to work

--- src/main/java/edu/cmu/lti/f13/hw4/hw4_wenyiwan/annotators/DocumentVectorAnnotator.java
+++ src/main/java/edu/cmu/lti/f13/hw4/hw4_wenyiwan/annotators/DocumentVectorAnnotator.java
@@ -1,5 +1,18 @@
 package edu.cmu.lti.f13.hw4.hw4_wenyiwan.annotators;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import org.apache.uima.UimaContext;
 import org.apache.uima.analysis_component.JCasAnnotator_ImplBase;
 import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
 import org.apache.uima.cas.FSIterator;
@@ -7,35 +20,88 @@ import org.apache.uima.jcas.JCas;
 import org.apache.uima.jcas.cas.IntegerArray;
 import org.apache.uima.jcas.cas.StringArray;
 import org.apache.uima.jcas.tcas.Annotation;
+import org.apache.uima.resource.ResourceInitializationException;
+import org.uimafit.util.FSCollectionFactory;
 
+import edu.cmu.lti.f13.hw4.hw4_wenyiwan.VectorSpaceRetrieval;
 import edu.cmu.lti.f13.hw4.hw4_wenyiwan.typesystems.Document;
+import edu.cmu.lti.f13.hw4.hw4_wenyiwan.typesystems.Token;
+import edu.stanford.nlp.ling.CoreLabel;
 
+/**
+ * @author wwy This is the class for document annotation.
+ */
 public class DocumentVectorAnnotator extends JCasAnnotator_ImplBase {
 
-	@Override
-	public void process(JCas jcas) throws AnalysisEngineProcessException {
+  HashSet<String> stopDic;
 
-		FSIterator<Annotation> iter = jcas.getAnnotationIndex().iterator();
-		if (iter.isValid()) {
-			iter.moveToNext();
-			Document doc = (Document) iter.get();
-			createTermFreqVector(jcas, doc);
-		}
+  /*
+   * The method initialize reads stop words from file and makes the dict of stop words.
+   */
+  @Override
+  public void initialize(UimaContext aContext) throws ResourceInitializationException {
+    super.initialize(aContext);
 
-	}
-	/**
-	 * 
-	 * @param jcas
-	 * @param doc
-	 */
+    stopDic = new HashSet<String>();
+    URL docUrl = VectorSpaceRetrieval.class.getResource("/stopwords.txt");
+    BufferedReader br;
+    try {
+      br = new BufferedReader(new InputStreamReader(docUrl.openStream()));
+      String sLine;
+      while ((sLine = br.readLine()) != null) {
+        sLine.replaceAll("[^a-zA-Z ]", "").toLowerCase().split("\\s+");
+        stopDic.add(sLine);
+      }
+      br.close();
+      br = null;
+    } catch (IOException e) {
+      throw new IllegalArgumentException("Error opening stopwords.txt");
+    }
 
-	private void createTermFreqVector(JCas jcas, Document doc) {
+  }
 
-		String docText = doc.getText();
-		
-		//TO DO: construct a vector of tokens and update the tokenList in CAS
-		
+  @Override
+  public void process(JCas jcas) throws AnalysisEngineProcessException {
 
-	}
+    FSIterator<Annotation> iter = jcas.getAnnotationIndex().iterator();
+    if (iter.isValid()) {
+      iter.moveToNext();
+      Document doc = (Document) iter.get();
+      createTermFreqVector(jcas, doc);
+    }
 
+  }
+
+  /**
+   * The method createTermFreqVector uses straightforward technique to do tokenization,
+   * namely, replacing non-alphabetical symbols, and split the string into tokens.
+   * Here, stop words are removed using the dictionary and a token list is build for each doc.
+   * @param jcas
+   * @param doc
+   */
+
+  private void createTermFreqVector(JCas jcas, Document doc) {
+    String docText = doc.getText();
+    // TO DO: construct a vector of tokens and update the tokenList in CAS
+    String[] tokens = docText.replaceAll("[^a-zA-Z ]", "").toLowerCase().split("\\s+");
+    Map<String, Integer> tokenCounts = new HashMap<String, Integer>();
+    for (String token : tokens) {
+      // remove stopwords
+      if (stopDic.contains(token.toLowerCase().trim()))
+        continue;
+      if (tokenCounts.containsKey(token))
+        tokenCounts.put(token, tokenCounts.get(token) + 1);
+      else
+        tokenCounts.put(token, 1);
+    }
+    ArrayList<Token> tokenArr = new ArrayList<Token>();
+    for (Entry<String, Integer> tokenCount : tokenCounts.entrySet()) {
+      Token token = new Token(jcas);
+      token.setFrequency(tokenCount.getValue());
+      token.setText(tokenCount.getKey());
+      token.addToIndexes();
+      tokenArr.add(token);
+    }
+    doc.setTokenList(FSCollectionFactory.createFSList(jcas, tokenArr));
+  }
 }
--- src/main/java/edu/cmu/lti/f13/hw4/hw4_wenyiwan/casconsumers/RetrievalEvaluator.java
+++ src/main/java/edu/cmu/lti/f13/hw4/hw4_wenyiwan/casconsumers/RetrievalEvaluator.java
@@ -2,7 +2,16 @@ package edu.cmu.lti.f13.hw4.hw4_wenyiwan.casconsumers;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+import java.util.TreeMap;
 
 import org.apache.uima.cas.CAS;
 import org.apache.uima.cas.CASException;
@@ -13,106 +22,198 @@ import org.apache.uima.jcas.cas.FSList;
 import org.apache.uima.resource.ResourceInitializationException;
 import org.apache.uima.resource.ResourceProcessException;
 import org.apache.uima.util.ProcessTrace;
+import org.uimafit.util.FSCollectionFactory;
 
 import edu.cmu.lti.f13.hw4.hw4_wenyiwan.typesystems.Document;
-
+import edu.cmu.lti.f13.hw4.hw4_wenyiwan.typesystems.Token;
 
 public class RetrievalEvaluator extends CasConsumer_ImplBase {
 
-	/** query id number **/
-	public ArrayList<Integer> qIdList;
+  /** query id number **/
+  public ArrayList<Integer> qIdList;
 
-	/** query and text relevant values **/
-	public ArrayList<Integer> relList;
+  /** query and text relevant values **/
+  public ArrayList<Integer> relList;
 
-		
-	public void initialize() throws ResourceInitializationException {
+  /** global word dictionary **/
+  private Set<String> globalWordDic;
 
-		qIdList = new ArrayList<Integer>();
+  /** list of documents **/
+  private List<Map<String, Integer>> docList;
 
-		relList = new ArrayList<Integer>();
+  /** list of ranks of answers **/
+  public ArrayList<Integer> rankList;
 
-	}
+  /** list of similarities **/
+  public ArrayList<Double> simList;
 
-	/**
-	 * TODO :: 1. construct the global word dictionary 2. keep the word
-	 * frequency for each sentence
-	 */
-	@Override
-	public void processCas(CAS aCas) throws ResourceProcessException {
+  /** positions of queries in documents **/
+  public ArrayList<Integer> qPos;
 
-		JCas jcas;
-		try {
-			jcas =aCas.getJCas();
-		} catch (CASException e) {
-			throw new ResourceProcessException(e);
-		}
+  /** positions of answers in documents **/
+  public HashMap<Integer, ArrayList<Integer>> aPos;
 
-		FSIterator it = jcas.getAnnotationIndex(Document.type).iterator();
-	
-		if (it.hasNext()) {
-			Document doc = (Document) it.next();
+  public int prevQueryID = 0;
 
-			//Make sure that your previous annotators have populated this in CAS
-			FSList fsTokenList = doc.getTokenList();
-			//ArrayList<Token>tokenList=Utils.fromFSListToCollection(fsTokenList, Token.class);
+  public void initialize() throws ResourceInitializationException {
 
-			qIdList.add(doc.getQueryID());
-			relList.add(doc.getRelevanceValue());
-			
-			//Do something useful here
+    qIdList = new ArrayList<Integer>();
+    relList = new ArrayList<Integer>();
+    globalWordDic = new HashSet<String>();
+    docList = new ArrayList<Map<String, Integer>>();
+    rankList = new ArrayList<Integer>();
+    simList = new ArrayList<Double>();
+    qPos = new ArrayList<Integer>();
+    aPos = new HashMap<Integer, ArrayList<Integer>>();
 
-		}
+  }
 
-	}
+  /**
+   * Global word dictionary is contructed. Word frequency for each sentence is recorded.
+   */
+  @Override
+  public void processCas(CAS aCas) throws ResourceProcessException {
 
-	/**
-	 * TODO 1. Compute Cosine Similarity and rank the retrieved sentences 2.
-	 * Compute the MRR metric
-	 */
-	@Override
-	public void collectionProcessComplete(ProcessTrace arg0)
-			throws ResourceProcessException, IOException {
+    JCas jcas;
+    try {
+      jcas = aCas.getJCas();
+    } catch (CASException e) {
+      throw new ResourceProcessException(e);
+    }
 
-		super.collectionProcessComplete(arg0);
+    FSIterator it = jcas.getAnnotationIndex(Document.type).iterator();
 
-		// TODO :: compute the cosine similarity measure
-		
-		
-		
-		// TODO :: compute the rank of retrieved sentences
-		
-		
-		
-		// TODO :: compute the metric:: mean reciprocal rank
-		double metric_mrr = compute_mrr();
-		System.out.println(" (MRR) Mean Reciprocal Rank ::" + metric_mrr);
-	}
+    if (it.hasNext()) {
+      Document doc = (Document) it.next();
 
-	/**
-	 * 
-	 * @return cosine_similarity
-	 */
-	private double computeCosineSimilarity(Map<String, Integer> queryVector,
-			Map<String, Integer> docVector) {
-		double cosine_similarity=0.0;
+      // Make sure that your previous annotators have populated this in CAS
+      FSList fsTokenList = doc.getTokenList();
 
-		// TODO :: compute cosine similarity between two sentences
-		
+      qIdList.add(doc.getQueryID());
+      relList.add(doc.getRelevanceValue());
+      rankList.add(0);
+      simList.add(0.0);
 
-		return cosine_similarity;
-	}
+      // Do something useful here
+      Map<String, Integer> token2Freq = new HashMap<String, Integer>();
+      for (Token token : FSCollectionFactory.create(fsTokenList, Token.class)) {
+        globalWordDic.add(token.getText()); // Global word dictionary is contructed.
+        token2Freq.put(token.getText(), token.getFrequency());
+      }
+      docList.add(token2Freq); // Word frequency for each sentence is recorded.
 
-	/**
-	 * 
-	 * @return mrr
-	 */
-	private double compute_mrr() {
-		double metric_mrr=0.0;
+      // System.out.println("cur doc id: " + doc.getQueryID());
+      if (doc.getQueryID() != prevQueryID) { // a new query
+        // System.out.println(prevQueryID);
+        prevQueryID = doc.getQueryID();
+        // record the postions of queries in the document.
+        qPos.add(docList.size() - 1);
+      } else { // not a new query
+        // record the postions of answers in the document.
+        if (aPos.containsKey(doc.getQueryID())) {
+          aPos.get(doc.getQueryID()).add(docList.size() - 1);
+        } else
+          aPos.put(doc.getQueryID(), new ArrayList<Integer>(Arrays.asList(docList.size() - 1)));
+      }
+    }
+  }
 
-		// TODO :: compute Mean Reciprocal Rank (MRR) of the text collection
-		
-		return metric_mrr;
-	}
+  /**
+   * Cosine Similarity computed and the retrieved sentences are ranked. The MRR metric is computed
+   * and displayed here.
+   */
+  @Override
+  public void collectionProcessComplete(ProcessTrace arg0) throws ResourceProcessException,
+          IOException {
+
+    super.collectionProcessComplete(arg0);
+
+    // TODO :: compute the cosine similarity measure
+    for (int pos : qPos) {
+      ArrayList<Integer> ansList = aPos.get(qIdList.get(pos));
+      for (int aPos : ansList) {// for each answer index, get the answer doc.
+        // double res = computeDice(documents.get(pos), documents.get(aPos));
+        // record the similarity result
+        simList.set(aPos, computeCosineSimilarity(docList.get(pos), docList.get(aPos)));
+      }
+    }
+
+    // TODO :: compute the rank of retrieved sentences
+    for (int pos : qPos) {
+      ArrayList<Integer> ansList = aPos.get(qIdList.get(pos));
+      // use the treemap to store similarity
+      TreeMap<Double, Integer> records = new TreeMap<Double, Integer>();
+      for (int aPos : ansList) {
+        records.put(simList.get(aPos), aPos);
+      }
+      ArrayList<Entry<Double, Integer>> keys = new ArrayList<Entry<Double, Integer>>(records.entrySet());
+      for (int i = 0; i < keys.size(); i++) {
+        rankList.set(keys.get(i).getValue(), keys.size() - i);
+      }
+    }
+
+    int prevQPos = Integer.MIN_VALUE;
+    for (int i = 0; i < docList.size(); i++) {
+      if (relList.get(i) == 99)
+        prevQPos = i;
+      if (relList.get(i) == 1) {
+        System.out.println(String.format("Score: %.10f\trank=%d\trel=%d qid=%d sent=%d",
+                simList.get(i), rankList.get(i), relList.get(i), qIdList.get(i), i - prevQPos));
+      }
+    }
+
+    // TODO :: compute the metric:: mean reciprocal rank
+    double metric_mrr = compute_mrr();
+    System.out.println(" (MRR) Mean Reciprocal Rank ::" + metric_mrr);
+  }
+
+  /**
+   * This is the method to compute cosine_similarity.
+   * @return cosine_similarity
+   */
+  private double computeCosineSimilarity(Map<String, Integer> queryVector,
+          Map<String, Integer> docVector) {
+    double cosine_similarity = 0.0;
+
+    // TODO :: compute cosine similarity between two sentences
+    double sqrSumOne = 0.0;
+    double sqrSumTwo = 0.0;
+    for (String word : globalWordDic) {
+      double freqOne = 0.0;
+      double freqTwo = 0.0;
+      if (queryVector.containsKey(word)) {
+        freqOne = queryVector.get(word);
+      }
+      if (docVector.containsKey(word)) {
+        freqTwo = docVector.get(word);
+      }
+      cosine_similarity += freqOne * freqTwo;
+      sqrSumOne += Math.pow(freqOne, 2);
+      sqrSumTwo += Math.pow(freqTwo, 2);
+    }
+    if (cosine_similarity != 0) {
+      cosine_similarity /= Math.pow((Math.sqrt(sqrSumOne) * Math.sqrt(sqrSumTwo)),1.6);
+    }
+    return cosine_similarity;
+  }
+
+  /**
+   * This is the method to compute MRR.
+   * @return mrr
+   */
+  private double compute_mrr() {
+    double metric_mrr = 0.0;
+
+    // TODO :: compute Mean Reciprocal Rank (MRR) of the text collection
+    for (int i = 0; i < docList.size(); i++) {
+      if (relList.get(i) == 1)
+        metric_mrr += 1.0 / rankList.get(i);
+    }
+    metric_mrr /= qPos.size();
+
+    // TODO :: compute Mean Reciprocal Rank (MRR) of the text collection
+
+    return metric_mrr;
+  }
 
 }
